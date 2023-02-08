42bcef9c1a62c11d22382e292236da484b9f47a6
Santhoshilakumarapu <santhoshilakumarapu@gmail.com>
2023-02-08 14:05:04 -0500

    moviecatalogservice

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,39 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import io.javabrains.moviecatalogservice.services.MovieInfo;
+import io.javabrains.moviecatalogservice.services.UserRatingInfo;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	MovieInfo movieInfo;
+	@Autowired
+	UserRatingInfo userRatingInfo;
+	
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+	UserRating userRating = userRatingInfo.getUserRating(userId);
+	
+	return userRating.getRatings().stream()
+					  .map(rating -> movieInfo.getCatalogItem(rating))
+	                  .collect(Collectors.toList());
+	}
+}
+
